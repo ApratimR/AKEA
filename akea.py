7aa66c9221c981d3c7876ab7a3bd5059fdfcef5e
ApratimR <apratimr55@gmail.com>
2020-09-17 22:20:44 +0530

    update #5

--- akea.py
+++ akea.py
@@ -1,3 +1,5 @@
+#NOTE delete test() later
+
 import numpy as np
 import base64
 
@@ -45,12 +47,34 @@ def decodeData(keyinput):
 	return temp
 
 
+initialVector = [24,39, 3,63, 7,46,16, 3,
+				 12,57,18,46,12,55,35,43,
+				 15, 6,25,53, 0,45,60,15,
+				 50,22,37,41,62,56,29,38,
+				  9,15,60, 2,55, 3,18, 8,
+				 54,10,28, 0,61,40,14,41,
+				 12, 2,40,27,23,43,36,62,
+				 32,60,45, 4,17,11,21, 4]
 
+permutations=np.genfromtxt("permutation.csv",delimiter=",",dtype=np.uint8)
+
+Combinations=np.genfromtxt("Combination.csv",delimiter=",",dtype=np.uint8)
+
+def keyexpander_subroutine1(parameter0):
+	
+	pass
+
+
+
+def keyexpander(keyinput):
+	
+	pass
 
 def test():
 	#simple naughty string test
 	string1 = "hell my name is __🐛🐛 1 aGFzT3duUHJvcGVydHk="
-	string2 = "aGVsbCBteSBuYW1lIGlzIF9f8J+Qm/CfkJsgMSBhR0Z6VDNkdVVISnZjR1Z5ZEhrPQ"
-	print(encodeData(string1))
-	print(decodeData(string2))
-test()
+	string2 = "YXBydGltdGlt"
+
+
+if __name__ == "__main__":
+	test()
