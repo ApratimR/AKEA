2c0e8fdd612fe45c91d12b310c4246e60198f3d4
ApratimR <apratimr55@gmail.com>
2020-09-20 05:27:11 +0530

    update #11 (subroutines for checking)

--- akea.py
+++ akea.py
@@ -63,10 +63,73 @@ substitutions = np.genfromtxt("substitution.csv",delimiter=",",dtype=np.uint8)
 
 messups = np.genfromtxt("messup.csv",delimiter=",",dtype=np.uint8)
 
-#this is called everytime before a key expansion is under process
-def verifyIntegrity():
-	status = False
+verifyValueForPermutation = [46, 57, 60, 40, 38, 62, 18, 14, 18, 37, 6, 9, 62, 23, 45, 21,
+ 3, 55, 32, 15, 3, 45, 54, 8, 40, 10, 28, 12, 46, 56, 39, 60, 4, 50, 11, 35, 41, 41, 16, 
+ 17, 55, 0, 29, 3, 27, 53, 2, 25, 61, 43, 36, 12, 22, 24, 4, 63, 60, 7, 0, 15, 2, 43, 15, 12]
 
+verifyValueForSubstitution = [22, 61, 56, 42, 47, 45, 41, 56, 35, 43, 58, 45, 35, 20, 17, 
+19, 52, 21, 5, 46, 27, 55, 59, 52, 18, 62, 30, 3, 36, 48, 54, 1, 26, 52, 59, 34, 20, 56, 
+58, 40, 51, 7, 53, 27, 12, 11, 13, 3, 35, 34, 11, 15, 14, 19, 50, 36, 8, 59, 55, 2, 37, 33, 31, 2]
+
+
+verifyValueForMessup = [2, 60, 22, 17, 23, 34, 7, 44, 29, 35, 20, 18, 8, 1, 34, 5, 22, 41,
+ 61, 35, 33, 30, 18, 25, 23, 33, 35, 61, 44, 14, 45, 27, 12, 48, 53, 24, 61, 50, 47, 6, 9,
+15, 21, 41, 28, 19, 17, 44, 47, 24, 55, 30, 39, 34, 25, 41, 47, 24, 55, 30, 39, 34, 25, 41]
+
+
+#this is called everytime before a key expansion is under process
+
+#permutation csv check
+def verifyIntegrity_subRoutine1():
+	status = False
+	temp = initialVector[:]
+	tempshadow = initialVector[:]
+
+	for temp1 in range(64):
+		for temp2 in range(64):
+			tempshadow[temp2] = temp[permutations[temp1][temp2]]
+		temp = tempshadow[:]
+	if(temp == verifyValueForPermutation):
+		status =True
+	else:
+		raise Exception("internal constants tampering")
+	return status
+
+
+#substitution csv check
+def verifyIntegrity_subRoutine2():
+	status = False
+	temp = initialVector[:]
+	for temp1 in range(64):
+		for temp2 in range(64):
+			temp[temp2]= substitutions[temp1][(temp[temp2])]
+
+	if (temp == verifyValueForSubstitution):
+		status = True
+	else:
+		raise Exception("internal constants tampering")
+	return status
+
+
+#messup csv check
+def verifyIntegrity_subRoutine3():
+	status = False
+	temp = initialVector[:]
+
+	for temp in range(64):
+		temp = (temp + messups[temp][:])%64
+
+	if (list(temp) == verifyValueForMessup):
+		status = True
+	else:
+		raise Exception("internal constants tampering")
+	return status
+
+def verifyIntegrity():
+	status = True
+	status = status and verifyIntegrity_subRoutine1()
+	status = status and verifyIntegrity_subRoutine2()
+	status = status and verifyIntegrity_subRoutine3()
 
 	return status
 
@@ -105,7 +168,7 @@ def test():
 	#simple naughty string test
 	string1 = "hell my name is __🐛🐛 1 aGFzT3duUHJvcGVydHk="
 	string2 = "YXBydGltdGlt"
-
+	print(verifyIntegrity())
 	#print(permutations)
 	#print(combinations)
 	#print(initialVector)
